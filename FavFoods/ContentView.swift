ef874ef31c30b48d39277747bd8e7edfe7c649ec
Jeremy <jeremygprawira@gmail.com>
2022-06-08 20:51:39 +0700

    Initial Commit

---
+++ FavFoods/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavFoods
+//
+//  Created by Jeremy Gerald Prawira on 08/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FavFoods/FavFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavFoodsApp.swift
+//  FavFoods
+//
+//  Created by Jeremy Gerald Prawira on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FavFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
